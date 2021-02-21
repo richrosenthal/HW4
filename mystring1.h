6d9024b23e782e08fa7366c55aa2379416de105e
Richard <42520886+richrosenthal@users.noreply.github.com>
2021-02-21 17:00:14 -0500

    initial commit

---
+++ mystring1.h
@@ -0,0 +1,45 @@
+//File: mystring1.h
+// ================
+// Interface file for user-defined String class.
+
+
+#ifndef _MYSTRING_H
+#define _MYSTRING_H
+#include<iostream>
+#include <cstring>  // for strlen(), etc.
+using namespace std;
+
+#define MAX_STR_LENGTH  200
+
+
+class String {
+public:
+  String();
+  String(const char s[]);  // a conversion constructor   
+  void append(const String &str);
+
+  // Relational operators
+  bool operator ==(const String &str) const;    
+  bool operator !=(const String &str) const;   
+  bool operator >(const String &str) const;    
+  bool operator <(const String &str) const;    
+
+  bool operator >=(const String &str) const; 
+
+ 
+  String operator +=(const String &str);      
+
+  void print(ostream &out) const;    
+  int length() const;
+
+  char operator [](int i) const;  // subscript operator  
+
+private:
+    char contents[MAX_STR_LENGTH+1];
+    int len;
+};
+
+ostream & operator<<(ostream &out, const String & r); // overload ostream operator "<<" - External!  
+
+
+#endif /* not defined _MYSTRING_H */
